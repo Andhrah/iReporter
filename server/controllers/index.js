9e6d24a6b043a69c3f023b179eca97153a399e39
Alexandra Collns <andracolexa@gmail.com>
2018-11-29 11:12:38 +0100

    ft(fetch-specific-red-flag): api endpoint to fetch a specific red-flag
    - create api endpoint to fetch a specific red-flag record
    
    [Delivers #162257126]

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,7 @@
 import getRedFlags from './incidents/red-flag/getRedFlags';
+import getSpecificRedFlag from './incidents/red-flag/getSpecificRedFlag';
 
 export default {
   getRedFlags,
+  getSpecificRedFlag,
 };
--- server/routes/red-flag.js
+++ server/routes/red-flag.js
@@ -7,4 +7,7 @@ const router = express.Router();
 // INDEX - displays a list of Red-Flag records
 router.get('/red-flags', controllers.getRedFlags);
 
+// SHOW - displays more information about a specific red-flag
+router.get('/red-flags/:id', controllers.getSpecificRedFlag);
+
 export default router;
