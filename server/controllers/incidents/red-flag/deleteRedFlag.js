23d67bfcb7cf2a4bbcc6aa6b4e0181716cc64fec
Alexandra Collns <andracolexa@gmail.com>
2018-11-30 05:45:56 +0100

    ft(delete-red-flag): endpoint for deleting red-flag
    - create endpoint for deleting red-flag
    
    [Deliver #162257129]

--- server/controllers/incidents/red-flag/deleteRedFlag.js
+++ server/controllers/incidents/red-flag/deleteRedFlag.js
@@ -1,14 +1,15 @@
 import db from '../../../models/red-flag';
 
 const deleteRedFlag = (req, res) => {
-  let redFlag = db.find(redFlagInDb => redFlagInDb.id === Number(req.paras.id));
+  const redFlag = db.find(redFlagInDb => redFlagInDb.id === Number(req.params.id));
   if (!redFlag) {
     return res.json({
       status: 404,
       error: 'Red-flag Not Found',
     });
   }
-  redFlag = '';
+  const index = db.indexOf(redFlag);
+  db.splice(index, 1);
   res.json({
     status: 200,
     data: [{
